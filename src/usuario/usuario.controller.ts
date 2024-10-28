b7df63084a1a30e3d3c685e10caf621d91269a3c
João Pedro <joao.medeiros@idus.com.br>
2024-10-28 08:40:59 -0300

    adding validation

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CriaUsuarioDTO } from './dto/CriaUsuario.dto';
 import { UsuarioRepository } from './usuario.repository';
 
 @Controller('/usuarios')
@@ -6,7 +7,7 @@ export class UsuarioController {
   constructor(private usuarioRepository: UsuarioRepository) {}
 
   @Post()
-  async criaUsuario(@Body() dadosDoUsuarios) {
+  async criaUsuario(@Body() dadosDoUsuarios: CriaUsuarioDTO) {
     this.usuarioRepository.salvar(dadosDoUsuarios);
     return dadosDoUsuarios;
   }
