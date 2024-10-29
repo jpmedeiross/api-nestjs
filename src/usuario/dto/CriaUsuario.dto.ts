1b06f2b89f8cc4341d354b77da852900bacfabc1
João Pedro <joao.medeiros@idus.com.br>
2024-10-29 08:32:22 -0300

    feat: creating validation for registration of just one email

--- src/usuario/dto/CriaUsuario.dto.ts
+++ src/usuario/dto/CriaUsuario.dto.ts
@@ -1,10 +1,12 @@
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { EmailUnico } from '../validacao/emailUnicoValidator';
 
 export class CriaUsuarioDTO {
   @IsNotEmpty({ message: 'O nome não pode ser vazio.' })
   nome: string;
 
   @IsEmail(undefined, { message: 'O e-mail é inválido.' })
+  @EmailUnico({ message: 'Digite um e-mail disponível. ' })
   email: string;
 
   @MinLength(6, { message: 'Senha deve possuir 6 caracteres.' })
